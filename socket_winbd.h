7d30152b4f963c01706a60c78e1880976a2e0822
Unamed <34691959+Unam3dd@users.noreply.github.com>
2020-05-09 11:40:15 +0200

    Update socket_winbd.h

--- socket_winbd.h
+++ socket_winbd.h
@@ -10,9 +10,3 @@
 
 
 int connect(SOCKET socks_instance, struct sockaddr_in session); // 0 = sucess | -1 = error
-int send_data(SOCKET socks_instance, char* buffer);
-int recv_data(SOCKET socks_instance, char* buffer, int len);
-void execute_process(SOCKET socks_instance, char* process);
-char* ghostbyaddr_ipv4(struct hostent* remote_host, const char* buffer_addr);
-servent* gservbyport(struct servent* get_service_name, int port);
-servent* gservbyname(struct servent* get_service_port, const char* name);
